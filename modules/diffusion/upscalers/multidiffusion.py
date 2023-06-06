1e7b8fe84ee6e3b375ec7a36ab4aec2db5370153
Isotr0py <2037008807@qq.com>
2023-06-06 10:31:14 +0800

    fix multidiffusion

--- modules/diffusion/upscalers/multidiffusion.py
+++ modules/diffusion/upscalers/multidiffusion.py
@@ -12,7 +12,6 @@ from diffusers import (
 from tqdm import tqdm
 from transformers import CLIPTextModel, CLIPTokenizer
 
-from lib.tensorrt.engine import UNet2DConditionModelEngine
 from .samplers import EulerAncestralSampler, KDPM2AncestralSampler
 
 
@@ -66,21 +65,8 @@ class Multidiffusion:
         views_batch_size: int,
         real_batch_size: int,
     ):
-        if (
-            isinstance(self.unet, UNet2DConditionModelEngine)
-            and views_batch_size != real_batch_size
-        ):
-            # expand latent to tensorrt batch size
-            shape = latent_model_input.shape[1:]
-            latent_align = torch.zeros(
-                views_batch_size * 2, *shape, device=latent_model_input.device
-            )
-            latent_align[: real_batch_size * 2, :, :, :] += latent_model_input
-            # repeat prompt_embeds for batch
-            prompt_embeds_align = torch.cat([prompt_embeds] * views_batch_size)
-        else:
-            prompt_embeds_align = torch.cat([prompt_embeds] * real_batch_size)
-            latent_align = latent_model_input
+        prompt_embeds_align = torch.cat([prompt_embeds] * real_batch_size)
+        latent_align = latent_model_input
         return latent_align, prompt_embeds_align
 
     def views_denoise_latent(
@@ -204,3 +190,22 @@ class Multidiffusion:
                         callback(step, timestep, latents)
 
         return 1 / 0.18215 * latents
+
+
+class MultidiffusionTensorRT(Multidiffusion):
+    def align_unet_inputs(
+        self,
+        latent_model_input: torch.Tensor,
+        prompt_embeds: torch.Tensor,
+        views_batch_size: int,
+        real_batch_size: int,
+    ):
+        # expand latent to tensorrt batch size
+        shape = latent_model_input.shape[1:]
+        latent_align = torch.zeros(
+            views_batch_size * 2, *shape, device=latent_model_input.device
+        )
+        latent_align[: real_batch_size * 2, :, :, :] += latent_model_input
+        # repeat prompt_embeds for batch
+        prompt_embeds_align = torch.cat([prompt_embeds] * views_batch_size)
+        return latent_align, prompt_embeds_align
